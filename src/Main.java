86883d1d957c6556f9b81ab175496669a0108cc8
mdpiekarz <monika.piekarz@mail.umcs.pl>
2024-03-06 18:20:28 +0100

    w1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(0, 0);
-        Point p2 = new Point(0, 10);
+        Point p2 = new Point(10, 0);
 
         Segment line = new Segment(p1, p2);
         System.out.println(line.toSvg());
